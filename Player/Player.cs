5e05f331657ec096c331ed89afcb7f670c94b69f
General-Monish <monissam28@gmail.com>
2024-11-27 00:05:26 +0530

    done with sounds

--- Player/Player.cs
+++ Player/Player.cs
@@ -6,6 +6,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float jumpForce = 100f;
     [SerializeField] float gravityModifier;
+    [SerializeField] ParticleSystem explosion;
+    [SerializeField] ParticleSystem dirtParticle;
+    [SerializeField] AudioClip jumpSound;
+    [SerializeField] AudioClip deathSound;
+
+    AudioSource playerAudio;
     Rigidbody rb;
     Animator anim;
     public bool isOnGround;
@@ -13,6 +19,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         Physics.gravity*=gravityModifier;
@@ -31,6 +38,8 @@ public class Player : MonoBehaviour
             anim.SetTrigger("Jump_trig");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1f);
         }
     }
 
@@ -39,12 +48,16 @@ public class Player : MonoBehaviour
         if (collision.collider.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.collider.CompareTag("OBS"))
         {
             isGameOver = true;
             Debug.Log("GameOver");
             anim.SetBool("Death_b", true);
             anim.SetInteger("DeathType_int", 1);
+            explosion.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(deathSound, 1f);
         }
     }
 }
