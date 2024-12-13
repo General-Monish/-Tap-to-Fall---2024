0e90ad09562d195d8ab9fcec09a128abd564a66a
General-Monish <monissam28@gmail.com>
2024-12-13 23:17:49 +0530

    added level displayto ball game

--- Ball/Smanager.cs
+++ Ball/Smanager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Smanager : MonoBehaviour
@@ -9,10 +10,12 @@ public class Smanager : MonoBehaviour
     float spawnPos = 9f;
     int enemyCount;
     int waveNum=2;
+    public TextMeshProUGUI levelText;
+    int levelCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelCount = 0;
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class Smanager : MonoBehaviour
             Instantiate(powerupPrefab, GenerateRandomPosition(), powerupPrefab.transform.rotation);
             waveNum++;
             SpawnEnemyWave(waveNum);
+            DisplayLevelNumber();
         }
     }
     // nothing
@@ -34,7 +38,7 @@ public class Smanager : MonoBehaviour
         {
             Instantiate(enemeyPrefab, GenerateRandomPosition(), enemeyPrefab.transform.rotation);
         }
-        
+        levelCount++;
     }
 
     private Vector3 GenerateRandomPosition()
@@ -43,5 +47,10 @@ public class Smanager : MonoBehaviour
         float spawnZ = Random.Range(-spawnPos, spawnPos);
         Vector3 randomPos = new Vector3(spawnX, 0, spawnZ);
         return randomPos;
+    }
+
+    private void DisplayLevelNumber()
+    {
+        levelText.text="Level: " + levelCount.ToString();
     }
 }
